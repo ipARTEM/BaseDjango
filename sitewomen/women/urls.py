17b8f97e2ed85cf12faa113710f642e14aaa709e
Artem Khimin <a.khimin@yandex.ru>
2024-05-05 23:52:03 +0300

    add FourDigitYearConverter

--- sitewomen/women/urls.py
+++ sitewomen/women/urls.py
@@ -1,5 +1,9 @@
-from  django.urls import  path, re_path
+from django.urls import path, re_path, register_converter
 from  . import  views
+from  . import  converters
+
+register_converter(converters.FourDigitYearConverter,'year4')
+
 
 urlpatterns = [
 
@@ -10,6 +14,8 @@ urlpatterns = [
 
     path('cats/<slug:cat_slug>/', views.categories_by_slug),  # http://127.0.0.1:8000/cat/2/
 
-    re_path(r'archive/(?P<year>[0-9]{4})',views.archive)
+    #re_path(r'archive/(?P<year>[0-9]{4})',views.archive)
+
+    path('archive/<year4:year>/', views.archive)
 
 ]
