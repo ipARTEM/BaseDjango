155f377d9cd6fe8eefe6e722d0c68f2e6a0dc3a9
Artem Khimin <a.khimin@yandex.ru>
2024-05-10 16:27:25 +0300

    add slugify

--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -1,16 +1,34 @@
 from django.http import HttpResponse, HttpResponseNotFound, Http404, HttpResponseRedirect, HttpResponsePermanentRedirect
 from django.shortcuts import render, redirect
+from django.template.defaultfilters import slugify
 from django.urls import reverse
 from django.template.loader import render_to_string
 
+menu=['О сайте', 'Добавить статью','Обратная связь','Войти']
+
+class MyClass:
+    def __init__(self, a,b):
+        self.a=a
+        self.b=b
 
 def index (request):
     # t=render_to_string('women/index.html')
     # return HttpResponse(t)
-    return render(request,'women/index.html')
+    data={
+        'title': 'главная страница?',
+        'main_title': '',
+        'menu':menu,
+        'float': 28.56,
+        'lst':[1,2,'abc', True],
+        'set':{1,2,3,2,5},
+        'dict':{'key_1':'value_1','key_2':'value_2'},
+        'obj':MyClass(10,20),
+        'url': slugify('The main page')
+    }
+    return render(request,'women/index.html',context=data)
 
 def about(request):
-    return render(request, 'women/about.html')
+    return render(request, 'women/about.html',{'title':'О сайте'})
 
 def categories(request,cat_id):
     return HttpResponse(f'<h1>Статьи по категориям</h1><p>id: {cat_id}</p>')
