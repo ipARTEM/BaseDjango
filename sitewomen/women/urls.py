59f5ceb11e37710f7e6bed9d4dfe57cff43fd8ba
Artem Khimin <a.khimin@yandex.ru>
2024-05-24 02:32:21 +0300

    add link

--- sitewomen/women/urls.py
+++ sitewomen/women/urls.py
@@ -1,30 +1,15 @@
 from django.urls import path, re_path, register_converter
-from  . import  views
-from  . import  converters
+from . import views
+from . import converters
 
-register_converter(converters.FourDigitYearConverter,'year4')
 
+register_converter(converters.FourDigitYearConverter, "year4")
 
 urlpatterns = [
-
-    path('',views.index, name='home'),       # http://127.0.0.1:8000
-    path('about',views.about, name='about'),
-
-    path('about2', views.about2, name='about2'),
-
-    path('addpage/', views.addpage,name='add_page'),
+    path('', views.index, name='home'),  # http://127.0.0.1:8000
+    path('about/', views.about, name='about'),
+    path('addpage/', views.addpage, name='add_page'),
     path('contact/', views.contact, name='contact'),
     path('login/', views.login, name='login'),
-
-    path('post/<int:post_id>/',views.show_post,name='post'),
-
-
-    path('cats/<int:cat_id>/', views.categories, name='cats_id'),  # http://127.0.0.1:8000/cat/2/
-
-    path('cats/<slug:cat_slug>/', views.categories_by_slug, name='cats'),  # http://127.0.0.1:8000/cat/2/
-
-    #re_path(r'archive/(?P<year>[0-9]{4})',views.archive)
-
-    path('archive/<year4:year>/', views.archive, name='archive')
-
+    path('post/<int:post_id>/', views.show_post, name='post'),
 ]
--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -1,109 +1,52 @@
 from django.http import HttpResponse, HttpResponseNotFound, Http404, HttpResponseRedirect, HttpResponsePermanentRedirect
 from django.shortcuts import render, redirect
-from django.template.defaultfilters import slugify
 from django.urls import reverse
 from django.template.loader import render_to_string
+from django.template.defaultfilters import slugify
 
-# menu=['О сайте', 'Добавить статью','Обратная связь','Войти']
-
-menu=[
-    {'title':'О сайте' ,'url_name': 'about'},
-    {'title': 'Добавить статью', 'url_name': 'add_page'},
-    {'title': 'Обратная связь', 'url_name': 'contact'},
-    {'title': 'Войти', 'url_name': 'login'},
+menu = [{'title': "О сайте", 'url_name': 'about'},
+        {'title': "Добавить статью", 'url_name': 'add_page'},
+        {'title': "Обратная связь", 'url_name': 'contact'},
+        {'title': "Войти", 'url_name': 'login'}
 ]
 
-class MyClass:
-    def __init__(self, a,b):
-        self.a=a
-        self.b=b
-
-
-data_db=[
-    {'id':1,'title':'Анджелина Джоли', 'content': 'Биография Анджелины Джоли', 'is_published': True},
-    {'id': 2, 'title': 'Науми Кембол', 'content': 'Биография Науми Кембол', 'is_published': False},
-    {'id': 3, 'title': 'Натали Потрмен', 'content': 'Биография Натали Портмен', 'is_published': True},
-
+data_db = [
+    {'id': 1, 'title': 'Анджелина Джоли', 'content': '''<h1>Анджелина Джоли</h1> (англ. Angelina Jolie[7], при рождении Войт (англ. Voight), ранее Джоли Питт (англ. Jolie Pitt); род. 4 июня 1975, Лос-Анджелес, Калифорния, США) — американская актриса кино, телевидения и озвучивания, кинорежиссёр, сценаристка, продюсер, фотомодель, посол доброй воли ООН.
+    Обладательница премии «Оскар», трёх премий «Золотой глобус» (первая актриса в истории, три года подряд выигравшая премию) и двух «Премий Гильдии киноактёров США».''',
+     'is_published': True},
+    {'id': 2, 'title': 'Марго Робби', 'content': 'Биография Марго Робби', 'is_published': False},
+    {'id': 3, 'title': 'Джулия Робертс', 'content': 'Биография Джулия Робертс', 'is_published': True},
 ]
 
-def index (request):
-    # t=render_to_string('women/index.html')
-    # return HttpResponse(t)
-    data={
+
+def index(request):
+    data = {
         'title': 'Главная страница',
-        'main_title': '',
-        'menu':menu,
+        'menu': menu,
         'posts': data_db,
-        'float': 28.56,
-        'lst':[1,2,'abc', True],
-        'set':{1,2,3,2,5},
-        'dict':{'key_1':'value_1','key_2':'value_2'},
-        'obj':MyClass(10,20),
-        'url': slugify('The main page')
     }
-    return render(request,'women/index.html',context=data)
+    return render(request, 'women/index.html', context=data)
+
 
 def about(request):
-    return render(request, 'base.html',{'title':'О сайте','menu': menu})
+    return render(request, 'women/about.html', {'title': 'О сайте', 'menu': menu})
 
-def about2(request):
-    return render(request, 'women/about.html',{'title':'О сайте2','menu': menu})
 
-def show_post(request,post_id):
-    return  HttpResponse(f' Отображение статьи с id {post_id}')
+def show_post(request, post_id):
+    return HttpResponse(f"Отображение статьи с id = {post_id}")
+
 
 def addpage(request):
-    return  HttpResponse(f'Добавление статьи')
+    return HttpResponse("Добавление статьи")
+
 
 def contact(request):
-    return  HttpResponse(f'Обратная связь')
+    return HttpResponse("Обратная связь")
+
 
 def login(request):
-    return  HttpResponse(f'Авторизация')
-
-def categories(request,cat_id):
-    return HttpResponse(f'<h1>Статьи по категориям</h1><p>id: {cat_id}</p>')
-
-def categories_by_slug(request,cat_slug):
-    if request.POST:
-        print(request.POST)
-    return  HttpResponse(f'<h1>Статьи по категориям</h1><p>slug: {cat_slug}</p>')
-
-def archive(request,year):
-
-    if year==2025:
-        return  redirect(index)
-
-    if year==2033:
-        return  redirect('home')
-
-    if year==2055:
-        return HttpResponsePermanentRedirect('/')
-
-    if year==2066:
-        return  HttpResponseRedirect('/')
-
-    if year==2077:
-        uri=reverse('cats',args=('misic',))
-        return redirect(uri)
-
-    if year==2088:
-        return redirect('cats','music')
-
-    if year==2000:
-        return  redirect('/',permanent=True)
-
-    # if year==2001:
-    #     return redirect('/')
+    return HttpResponse("Авторизация")
 
 
-    if year>2024:
-        raise Http404()
-    if year<1000:
-        return redirect('/')
-
-    return  HttpResponse(f'<h1>Архив по годам</h1><p>{year}</p>')
-
-def page_not_found(request,exception):
-    return HttpResponseNotFound(f'<h1>Страница не найдена</h1>')
-
+def page_not_found(request, exception):
+    return HttpResponseNotFound("<h1>Страница не найдена</h1>")
