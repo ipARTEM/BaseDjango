8644a80a237ddb0a8112cab18c26342906a4c4d9
Artem Khimin <a.khimin@yandex.ru>
2024-06-04 22:40:35 +0300

    tags

--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -18,12 +18,20 @@ data_db = [
     {'id': 3, 'title': 'Джулия Робертс', 'content': 'Биография Джулия Робертс', 'is_published': True},
 ]
 
+cats_db=[
+    {'id': 1,'name':'Актрисы'},
+    {'id': 2, 'name': 'Певицы'},
+    {'id': 3, 'name': 'Спортсменки'},
+
+]
+
 
 def index(request):
     data = {
         'title': 'Главная страница',
         'menu': menu,
         'posts': data_db,
+        'cat_selected':0,
     }
     return render(request, 'women/index.html', context=data)
 
@@ -46,6 +54,15 @@ def contact(request):
 
 def login(request):
     return HttpResponse("Авторизация")
+
+def show_category(request, cat_id):
+    data = {
+        'title': 'Отображение по рубрикам',
+        'menu': menu,
+        'posts': data_db,
+        'cat_selected':cat_id,
+    }
+    return render(request, 'women/index.html', context=data)
 
 
 def page_not_found(request, exception):
