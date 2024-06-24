1a7f023b7f777e6659ad577134dcf9fc84580e85
Artem Khimin <a.khimin@yandex.ru>
2024-06-24 22:59:45 +0300

    Class AbstractUser

--- sitewomen/sitewomen/settings.py
+++ sitewomen/sitewomen/settings.py
@@ -145,3 +145,19 @@ AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
     'users.authentication.EmailAuthBackend',
 ]
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+
+EMAIL_HOST = "smtp.yandex.ru"
+EMAIL_PORT = 465
+EMAIL_HOST_USER = "djangocourse@yandex.ru"
+EMAIL_HOST_PASSWORD = "bnufhkwcripaunvu"
+EMAIL_USE_SSL = True
+
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+SERVER_EMAIL = EMAIL_HOST_USER
+EMAIL_ADMIN = EMAIL_HOST_USER
+
+AUTH_USER_MODEL = 'users.User'
+
+DEFAULT_USER_IMAGE = MEDIA_URL + 'users/default.png'
